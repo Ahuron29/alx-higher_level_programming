9f8b331e75aecbbdea2db9a2e92e06b55759dae6
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-07-06 02:44:00 +0000

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@ A function that reads a text file (UTF8) and prints it to stdout:
 
 
 def read_file(filename=""):
-        """calling the function"""
-            with open(filename, encoding="UTF-8") as readfile:
-                        print(readfile.read(), end="")
+    """calling the function"""
+    with open(filename, encoding="UTF-8") as readfile:
+        print(readfile.read(), end="")
