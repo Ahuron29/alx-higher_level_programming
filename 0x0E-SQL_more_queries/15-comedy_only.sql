764090d4d74a3bc3ab44e67390d0113067d6cc89
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-08-11 02:02:55 +0000

    Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,9 @@
--- Lists all genres of the show Dexter in the database hbtn_0d_tvshows.
--- Records are ordered by ascending genre name.
-SELECT g.`name`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
-       INNER JOIN `tv_shows` AS t
-       ON t.`id` = s.`show_id`
-       WHERE t.`title` = "Dexter"
- ORDER BY g.`name`;
+-- Script that lists genres of Dexter show
+SELECT tv_shows.title -- Query to join tables
+FROM tv_shows
+     JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
