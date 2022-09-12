0b4aff59f1a831ddaee8c9d7fa2e243a355fb87a
Emmanuel Ahuron <65813159+Ahuron29@users.noreply.github.com>
2022-09-12 11:54:34 +0000

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const msg = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(msg[i]);
+const myArray = [
+  'C is fun',
+  'Python is cool',
+  'Javascript is amazing'
+];
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
 }
